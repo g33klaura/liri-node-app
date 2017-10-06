98818110467c53af2c0a3338822804f3a68aada2
Laura Boetcher <g33klaura@gmail.com>
2017-10-06 18:20:21 -0400

    Add Twitter keys

---
+++ assets/keys.js
@@ -0,0 +1,18 @@
+console.log('this is loaded');
+
+// Keys for Twitter
+var twitterKeys = {
+	consumer_key: 'S8stx48K48Rf8AXx4Gv8YT0Zl',
+	consumer_secret: '4dc443Pbygr6dQsLwwaDYPlZrC9IePRhmPcqDnPPxnTuszCCIQ',
+	access_token_key: '916407260955369472-wi7lWSKFPSaMP01YAHNuPOVyEHrMMBu',
+	access_token_secret: 'MpTPXL5HEoFFAzrwyPVQwYyjBxIvuc44P5gOdrdEVvlTa'
+}
+
+module.exports = twitterKeys;
+
+
+// Keys for Spotify
+
+
+
+// Keys for OMDB
