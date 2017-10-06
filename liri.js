c3debc68f4a5e80c39d693b794ae42a751639da2
Laura Boetcher <g33klaura@gmail.com>
2017-10-06 18:09:04 -0400

    Initial script and ignore files

---
+++ liri.js
@@ -0,0 +1,24 @@
+// JavaScript for LIRI-Bot
+// Steps to complete:
+// []
+
+// REQUIREMENTS ====================
+// 
+
+
+
+
+// VARIABLES ====================
+// 
+
+
+
+
+// FUNCTIONS ====================
+//
+
+
+
+
+// MAIN PROCESS ====================
+// 
