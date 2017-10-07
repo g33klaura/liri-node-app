17c35bddb6fd4967bd51f2e40ae1b400078bbb24
Laura Boetcher <g33klaura@gmail.com>
2017-10-06 21:41:33 -0400

    Update export object; pseudo code space for other 2 api keys

--- assets/keys.js
+++ assets/keys.js
@@ -1,14 +1,12 @@
 console.log('this is loaded');
 
 // Keys for Twitter
-var twitterKeys = {
+var twitterKey = {
 	consumer_key: 'S8stx48K48Rf8AXx4Gv8YT0Zl',
 	consumer_secret: '4dc443Pbygr6dQsLwwaDYPlZrC9IePRhmPcqDnPPxnTuszCCIQ',
 	access_token_key: '916407260955369472-wi7lWSKFPSaMP01YAHNuPOVyEHrMMBu',
 	access_token_secret: 'MpTPXL5HEoFFAzrwyPVQwYyjBxIvuc44P5gOdrdEVvlTa'
-}
-
-module.exports = twitterKeys;
+};
 
 
 // Keys for Spotify
@@ -16,3 +14,13 @@ module.exports = twitterKeys;
 
 
 // Keys for OMDB
+
+
+
+// Export object
+// Q: OK THESE AREN'T BOTH SAME LABELS???
+module.exports = {
+	twitter: twitterKey,
+	// spotify: spotKey,
+	// omdb: omdbKey
+}
