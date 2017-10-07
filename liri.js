85afb5ca8ed76d3f906884f012a6503f80d57ddc
Laura Boetcher <g33klaura@gmail.com>
2017-10-06 21:56:18 -0400

    Add switch statement for command cases

--- liri.js
+++ liri.js
@@ -18,24 +18,51 @@
 // REQUIREMENTS ====================
 // 
 
-const keys = require('assets/keys.js');
+const keys = require('./assets/keys.js');
+	// console.log(keys);
+
 // const fs = require('fs');
 
-console.log(keys);
+
 
 
 
 // VARIABLES ====================
 // 
 
+// Stores command entered (ie. 'my-tweets', 'movie-this', etc.)
+let command = process.argv[2];
 
 
 
 // FUNCTIONS ====================
 //
 
-
+// Will need functions for each command needed
 
 
 // MAIN PROCESS ====================
 // 
+
+// Need switch statment (or if/else) for each command possibility
+
+switch (command) {
+	case 'my-tweets':
+		// do this;
+		break;
+	case 'spotify-this-song':
+		// do this;
+		break;
+	case 'movie-this':
+		// do this;
+		break;
+	case 'do-what-it-says':
+		// do this;
+		break;
+	default:
+		console.log('Invalid command');
+		break;
+}
+
+
+
