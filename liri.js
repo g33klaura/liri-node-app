41608b7bb8d8d7fb956c44d8b4fc5d3f4ed38727
Laura Boetcher <g33klaura@gmail.com>
2017-10-06 21:41:48 -0400

    Add requirement code for keys.js

--- liri.js
+++ liri.js
@@ -1,10 +1,27 @@
 // JavaScript for LIRI-Bot
 // Steps to complete:
-// []
+/* *Starting @ step 7*
+	[] at top of this script, write code needed to grab data from keys.js
+	[] need these commands to run:
+		[] my-tweets
+		[] spotify-this-song
+		[] movie-this
+		[] do-what-it-says
+	[] check evernote list for what each command needs to do
+	[] 
+
+
+*/
+
+
 
 // REQUIREMENTS ====================
 // 
 
+const keys = require('assets/keys.js');
+// const fs = require('fs');
+
+console.log(keys);
 
 
 
